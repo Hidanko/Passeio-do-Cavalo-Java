786925a1dc6fcaeeba40563b892a0200fc216110
Gustavo Nemeth <hidan_nemeth@hotmail.com>
2019-02-13 20:41:02 -0200

    First commit

---
+++ src/main/java/br/com/nemeth/chess/App.java
@@ -0,0 +1,14 @@
+package br.com.nemeth.chess;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Cavalo cavalo = new Cavalo();
+       cavalo.run();
+    }
+}
---
+++ src/main/java/br/com/nemeth/chess/Posicao.java
@@ -0,0 +1,28 @@
+package br.com.nemeth.chess;
+
+public class Posicao {
+
+	int x;
+	int y;
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public Posicao(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
